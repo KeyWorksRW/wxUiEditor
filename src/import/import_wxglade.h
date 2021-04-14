9835fad212ef9f0d1cd821de525f0ed309e68232
Randalphwa <38287198+Randalphwa@users.noreply.github.com>
2021-04-14 10:26:36 -0700

    Add skeleton wxGlade import class

---
+++ src/import/import_wxglade.h
@@ -0,0 +1,26 @@
+/////////////////////////////////////////////////////////////////////////////
+// Purpose:   Import a wxGlade file
+// Author:    Ralph Walden
+// Copyright: Copyright (c) 2021 KeyWorks Software (Ralph Walden)
+// License:   Apache License -- see ../../LICENSE
+/////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include "node_classes.h"
+
+#include "import_xml.h"        // ImportXML -- Base class for XML importing
+
+class WxGlade : public ImportXML
+{
+public:
+    WxGlade();
+
+    bool Import(const ttString& filename, bool write_doc = true) override;
+
+protected:
+
+    NodeSharedPtr CreateGladeNode(pugi::xml_node& xml_obj, Node* parent, Node* sizeritem = nullptr);
+
+private:
+};

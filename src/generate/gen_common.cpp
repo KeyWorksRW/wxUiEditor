b45a58ef6a934c3b83351e38e1ca50c23ed448c7
Randalphwa <38287198+Randalphwa@users.noreply.github.com>
2021-05-21 09:03:03 -0700

    Re-enable GenFormSettings() conditional code
    The wxDialog generator no longer calls this function, so we don't need to conditionalize any of the code.

--- src/generate/gen_common.cpp
+++ src/generate/gen_common.cpp
@@ -773,12 +773,8 @@ ttlib::cstr GenFormSettings(Node* node)
 
     if (!node->isGen(gen_PanelForm) && !node->isGen(gen_wxToolBar))
     {
-        auto min_size = node->prop_as_wxSize(prop_minimum_size);
         auto max_size = node->prop_as_wxSize(prop_maximum_size);
-
-#if 0
-// REVIEW: [KeyWorks - 05-20-2021] This is definitely wrong for a wxDialog (see issue #242) -- is it valid for a
-// wxFrame or any other type of form?
+        auto min_size = node->prop_as_wxSize(prop_minimum_size);
         if (min_size != wxDefaultSize || max_size != wxDefaultSize)
         {
             code << "SetSizeHints(";
@@ -792,7 +788,6 @@ ttlib::cstr GenFormSettings(Node* node)
 
             code << ");";
         }
-#endif
 
         if (node->HasValue(prop_icon))
         {

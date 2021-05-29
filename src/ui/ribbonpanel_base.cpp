21162d22b9d928205cbb39683bc9fad185aef484
Randalphwa <38287198+Randalphwa@users.noreply.github.com>
2021-05-29 06:25:49 -0700

    Add wxContextMenuEvent to ribbon tool after Window forms
    This needs a separator -- but first #272 must be fixed.

--- src/ui/ribbonpanel_base.cpp
+++ src/ui/ribbonpanel_base.cpp
@@ -132,6 +132,7 @@ RibbonPanelBase::RibbonPanelBase(wxWindow* parent, wxWindowID id) : wxPanel()
         forms_bar_windows->AddTool(gen_PanelForm, GetImgFromHdr(wxPanel_png, sizeof(wxPanel_png)), wxString::FromUTF8("wxPanel"), wxRIBBON_BUTTON_NORMAL);
         forms_bar_windows->AddTool(CreateNewFrame, GetImgFromHdr(wxFrame_png, sizeof(wxFrame_png)), wxString::FromUTF8("wxFrame"), wxRIBBON_BUTTON_NORMAL);
         forms_bar_windows->AddTool(NewPopupWin, GetImgFromHdr(wxPopupTransientWindow_png, sizeof(wxPopupTransientWindow_png)), wxString::FromUTF8("wxPopupTransientWindow"), wxRIBBON_BUTTON_NORMAL);
+        forms_bar_windows->AddTool(gen_wxContextMenuEvent, GetImgFromHdr(menuitem_png, sizeof(menuitem_png)), wxString::FromUTF8("wxPopupTransientWindow"), wxRIBBON_BUTTON_NORMAL);
     }
     forms_bar_windows->Realize();
 
@@ -400,8 +401,6 @@ RibbonPanelBase::RibbonPanelBase(wxWindow* parent, wxWindowID id) : wxPanel()
     }
     other_bar_html->Realize();
     m_rbnBar->Realize();
-
-    SetSizerAndFit(parent_sizer);
 
     SetSizerAndFit(parent_sizer);
 

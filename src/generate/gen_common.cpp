0727d0782ab65b4afa970cc76963b89c82676419
Randalphwa <38287198+Randalphwa@users.noreply.github.com>
2021-08-24 17:40:15 -0700

    Add and fix additional code for generating dialog units

--- src/generate/gen_common.cpp
+++ src/generate/gen_common.cpp
@@ -254,7 +254,16 @@ void GenPos(Node* node, ttlib::cstr& code)
 {
     auto point = node->prop_as_wxPoint(prop_pos);
     if (point.x != -1 || point.y != -1)
-        code << "wxPoint(" << point.x << ", " << point.y << ")";
+    {
+        if (node->prop_as_string(prop_pos).contains("d", tt::CASE::either))
+        {
+            code << ", ConvertPixelsToDialog(wxPoint(" << point.x << ", " << point.y << "))";
+        }
+        else
+        {
+            code << "wxPoint(" << point.x << ", " << point.y << ")";
+        }
+    }
     else
         code << "wxDefaultPosition";
 }
@@ -263,7 +272,16 @@ void GenSize(Node* node, ttlib::cstr& code)
 {
     auto size = node->prop_as_wxSize(prop_size);
     if (size != wxDefaultSize)
-        code << "wxSize(" << size.x << ", " << size.y << ")";
+    {
+        if (node->prop_as_string(prop_size).contains("d", tt::CASE::either))
+        {
+            code << ", ConvertPixelsToDialog(wxSize(" << size.x << ", " << size.y << "))";
+        }
+        else
+        {
+            code << "wxSize(" << size.x << ", " << size.y << ")";
+        }
+    }
     else
         code << "wxDefaultSize";
 }
@@ -368,7 +386,7 @@ void GeneratePosSizeFlags(Node* node, ttlib::cstr& code, bool uses_def_validator
             code << ", wxDefaultPosition";
             isPosSet = true;
         }
-        if (node->prop_as_string(prop_pos).contains("d", tt::CASE::either))
+        if (node->prop_as_string(prop_size).contains("d", tt::CASE::either))
         {
             code << ", ConvertPixelsToDialog(wxSize(" << size.x << ", " << size.y << "))";
         }

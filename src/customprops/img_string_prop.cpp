d8dcc0d7b736b4c8454cd2a318fb216754c6744e
Randalphwa <38287198+Randalphwa@users.noreply.github.com>
2021-10-21 15:44:21 -0700

    Don't attempt to change to art directory if it doesn't exist
    It might have a default location, or it might have been moved or renamed.

--- src/customprops/img_string_prop.cpp
+++ src/customprops/img_string_prop.cpp
@@ -33,7 +33,11 @@ bool ImageDialogAdapter::DoShowDialog(wxPropertyGrid* propGrid, wxPGProperty* WX
         ttSaveCwd cwd;
         if (wxGetApp().GetProject()->HasValue(prop_original_art))
         {
-            wxFileName::SetCwd(wxGetApp().GetOriginalArtDir());
+            auto dir = wxGetApp().GetOriginalArtDir();
+            if (dir.dir_exists())
+            {
+                wxFileName::SetCwd(dir);
+            }
         }
 
         wxString pattern;
@@ -61,11 +65,11 @@ bool ImageDialogAdapter::DoShowDialog(wxPropertyGrid* propGrid, wxPGProperty* WX
     else if (m_img_props.type.contains("XPM") || m_img_props.type.contains("Header"))
     {
         ttSaveCwd cwd;
-        if (wxGetApp().GetProject()->HasValue(prop_converted_art))
+        if (wxGetApp().GetProject()->HasValue(prop_converted_art) && wxGetApp().GetOriginalArtDir().dir_exists())
         {
             wxFileName::SetCwd(wxGetApp().GetConvertedArtDir());
         }
-        else if (wxGetApp().GetProject()->HasValue(prop_original_art))
+        else if (wxGetApp().GetProject()->HasValue(prop_original_art) && wxGetApp().GetOriginalArtDir().dir_exists())
         {
             wxFileName::SetCwd(wxGetApp().GetOriginalArtDir());
         }

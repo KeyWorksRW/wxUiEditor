cf4fdc8c90c64d734f057e7be670f29375284f89
Randalphwa <38287198+Randalphwa@users.noreply.github.com>
2021-11-07 05:40:57 -0800

    Create modules for resForm and resCtrl utility functions

---
+++ src/winres/form_utils.cpp
@@ -0,0 +1,10 @@
+/////////////////////////////////////////////////////////////////////////////
+// Purpose:   resForm utility functions
+// Author:    Ralph Walden
+// Copyright: Copyright (c) 2020-2021 KeyWorks Software (Ralph Walden)
+// License:   Apache License -- see ../../LICENSE
+/////////////////////////////////////////////////////////////////////////////
+
+#include "winres_form.h"
+
+#include "node_creator.h"  // NodeCreator -- Class used to create nodes

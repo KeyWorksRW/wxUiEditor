aba460ab75a61d804e17732f32ab64a78ff4a083
Randalphwa <38287198+Randalphwa@users.noreply.github.com>
2021-12-01 13:43:54 -0800

    Tweaks to improve clang-cl compilation
    Unfortunately, link fails because the linker can't locate wmemchr.

--- src/pch.cpp
+++ src/pch.cpp
@@ -1,3 +1,9 @@
-// Thie file is used to create a pre-compiled header for use in the entire project (required by MSVC)
+// Thie file is used to create a pre-compiled header for use in the entire project (required by clang-cl using ttBld ninja
+// script)
 
 #include "pch.h"
+
+#pragma comment(lib, "winspool.lib")
+#pragma comment(lib, "oleaut32.lib")
+#pragma comment(lib, "ole32.lib")
+#pragma comment(lib, "uuid.lib")

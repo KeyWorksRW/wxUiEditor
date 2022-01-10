0e1b12214e0c8580f2c94d41375067813d80fe74
Randalphwa <38287198+Randalphwa@users.noreply.github.com>
2022-01-10 05:57:16 -0800

    Replace Startup dialog with an entirely new dialog

--- src/ui/wxui_code.cmake
+++ src/ui/wxui_code.cmake
@@ -31,6 +31,7 @@ set (wxue_generated_code
     ${CMAKE_CURRENT_LIST_DIR}/newribbon_base.cpp
     ${CMAKE_CURRENT_LIST_DIR}/optionsdlg_base.cpp
     ${CMAKE_CURRENT_LIST_DIR}/ribbonpanel_base.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/startupdlg_base.cpp
     ${CMAKE_CURRENT_LIST_DIR}/ui_images.cpp
 
 )

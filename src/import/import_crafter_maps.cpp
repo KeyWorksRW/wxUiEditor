bb9b34c9b614853f49fcceccbec548e49940e948
Randalphwa <38287198+Randalphwa@users.noreply.github.com>
2022-01-19 08:34:15 -0800

    Update which controls are verified, add more prop conversion

--- src/import/import_crafter_maps.cpp
+++ src/import/import_crafter_maps.cpp
@@ -31,41 +31,41 @@ std::map<int, GenEnum::GenName> g_map_id_generator = {
     { 4418, gen_wxStaticLine },     // verified
     { 4419, gen_wxSlider },         // verified
     { 4420, gen_wxGauge },          // verified
-    { 4421, gen_wxDialog },
-    { 4422, gen_wxTreeCtrl },
-    { 4423, gen_wxHtmlWindow },
-    { 4424, gen_wxRichTextCtrl },
-    { 4425, gen_wxCheckListBox },
+    { 4421, gen_wxDialog },         // verified
+    { 4422, gen_wxTreeCtrl },       // verified (wxCrafter doesn't support any styles)
+    { 4423, gen_wxHtmlWindow },     // verified (wxUiEditor doesn't support URL)
+    { 4424, gen_wxRichTextCtrl },   // verified
+    { 4425, gen_wxCheckListBox },   // verified
     { 4426, gen_wxGrid },
-    { 4427, gen_wxToggleButton },
-    { 4428, gen_wxSearchCtrl },
-    { 4429, gen_wxColourPickerCtrl },
-    { 4430, gen_wxFontPickerCtrl },
-    { 4431, gen_wxFilePickerCtrl },
-    { 4432, gen_wxDirPickerCtrl },
-    { 4433, gen_wxDatePickerCtrl },
-    { 4434, gen_wxCalendarCtrl },
-    { 4435, gen_wxScrollBar },
-    { 4436, gen_wxSpinCtrl },
-    { 4437, gen_wxSpinButton },
-    { 4438, gen_wxHyperlinkCtrl },
-    { 4439, gen_wxGenericDirCtrl },
-    { 4440, gen_wxScrolledWindow },
+    { 4427, gen_wxToggleButton },      // verified
+    { 4428, gen_wxSearchCtrl },        // verified
+    { 4429, gen_wxColourPickerCtrl },  // verified
+    { 4430, gen_wxFontPickerCtrl },    // verified
+    { 4431, gen_wxFilePickerCtrl },    // verified
+    { 4432, gen_wxDirPickerCtrl },     // verified
+    { 4433, gen_wxDatePickerCtrl },    // verified
+    { 4434, gen_wxCalendarCtrl },      // verified
+    { 4435, gen_wxScrollBar },         // verified
+    { 4436, gen_wxSpinCtrl },          // verified
+    { 4437, gen_wxSpinButton },        // verified
+    { 4438, gen_wxHyperlinkCtrl },     // verified
+    { 4439, gen_wxGenericDirCtrl },    // verified
+    { 4440, gen_wxScrolledWindow },    // verified
     { 4441, gen_BookPage },
-    { 4442, gen_wxNotebook },
-    { 4443, gen_wxToolbook },
-    { 4444, gen_wxListbook },
-    { 4445, gen_wxChoicebook },
-    { 4446, gen_wxTreebook },
-    { 4447, gen_wxSplitterWindow },
-    { 4448, gen_wxPanel /* used as a child of a wxSplitterWindow */ },
-    { 4449, gen_wxStaticBoxSizer },
-    { 4450, gen_wxWizard },
-    { 4451, gen_wxWizardPageSimple },
-    { 4452, gen_wxGridSizer },
-    { 4453, gen_wxGridBagSizer },
-    { 4454, gen_spacer },
-    { 4456, gen_wxAuiNotebook },
+    { 4442, gen_wxNotebook },          // verified
+    { 4443, gen_wxToolbook },          // verified
+    { 4444, gen_wxListbook },          // verified
+    { 4445, gen_wxChoicebook },        // verified
+    { 4446, gen_wxTreebook },          // verified
+    { 4447, gen_wxSplitterWindow },    // verified
+    { 4448, gen_wxPanel },             // verified -- used as a child of wxSplitterWindow
+    { 4449, gen_wxStaticBoxSizer },    // verified
+    { 4450, gen_wxWizard },            // verified
+    { 4451, gen_wxWizardPageSimple },  // verified
+    { 4452, gen_wxGridSizer },         // verified
+    { 4453, gen_wxGridBagSizer },      // verified
+    { 4454, gen_spacer },              // verified
+    { 4456, gen_wxAuiNotebook },       // verified
     { 4457, gen_wxMenuBar },
     { 4458, gen_wxMenu },
     { 4459, gen_wxMenuItem },
@@ -75,8 +75,8 @@ std::map<int, GenEnum::GenName> g_map_id_generator = {
     { 4463, gen_wxAuiToolBar },
     { 4464, gen_wxStatusBar },
     { 4465, gen_CustomControl },
-    { 4466, gen_wxStyledTextCtrl },
-    { 4467, gen_wxStdDialogButtonSizer },
+    { 4466, gen_wxStyledTextCtrl },        // verified
+    { 4467, gen_wxStdDialogButtonSizer },  // verified
     { 4469, gen_wxDataViewListCtrl },
     { 4470, gen_wxDataViewTreeCtrl },
     { 4471, gen_wxBannerWindow },
@@ -92,8 +92,8 @@ std::map<int, GenEnum::GenName> g_map_id_generator = {
     { 4490, gen_wxRibbonPanel },
     { 4504, gen_toolSeparator },
     { 4509, gen_wxTreeListCtrl },
-    { 4511, gen_wxSimplebook },
-    { 4513, gen_wxToggleButton },
+    { 4511, gen_wxSimplebook },    // verified
+    { 4513, gen_wxToggleButton },  // verified
     { 4519, gen_wxAnimationCtrl },
     { 4520, gen_wxBitmapComboBox },
     { 4521, gen_wxRearrangeCtrl },
@@ -154,22 +154,38 @@ std::map<std::string, GenEnum::PropName> g_map_crafter_props = {
     { "class decorator", prop_class_decoration },
     { "class name", prop_derived_class },
     { "default button", prop_default },
+    { "default path", prop_defaultfolder },
     { "enable window persistency", prop_persist },
     { "fg colour", prop_foreground_colour },
     { "file", prop_derived_file },
+    { "filter index", prop_defaultfilter },
     { "growable columns", prop_growablecols },
     { "growable rows", prop_growablerows },
     { "help string", prop_statusbar },
     { "horizontal gap", prop_hgap },
+    { "hover colour", prop_hover_color },
+    { "html code", prop_html_content },
     { "include file", prop_derived_header },
     { "inherited class", prop_class_name },
     { "major dimension", prop_majorDimension },
     { "max length", prop_maxlength },
     { "max value", prop_max },
     { "min value", prop_min },
+    { "minimum pane size", prop_min_pane_size },
     { "minimum size", prop_minimum_size },
+    { "normal colour", prop_normal_color },
+    { "page size", prop_pagesize },
+    { "sash gravity", prop_sashgravity },
+    { "sash position", prop_sashpos },
+    { "scroll rate x", prop_scroll_rate_x },
+    { "scroll rate y", prop_scroll_rate_y },
+    { "show cancel button", prop_cancel_button },
+    { "show hidden files", prop_show_hidden },
+    { "show search button", prop_search_button },
+    { "split mode", prop_splitmode },
     { "text hint", prop_hint },
     { "vertical gap", prop_vgap },
+    { "visited colour", prop_visited_color },
 
     // { "minimum", prop_min_size},
 

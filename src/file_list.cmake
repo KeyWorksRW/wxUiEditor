a93c08423680d72447d6aa6373d07a3126986fb4
Randalphwa <38287198+Randalphwa@users.noreply.github.com>
2022-02-15 05:21:34 -0800

    Minor reformatting, fix pugixml source
    All lines need to start with ${CMAKE_CURRENT_LIST_DIR}/ even if the file is not in a higher directory then the current one. This is so that it doesn't matter where CMakeLists.txt is run from.

--- src/file_list.cmake
+++ src/file_list.cmake
@@ -20,27 +20,26 @@ set (file_list
     ${CMAKE_CURRENT_LIST_DIR}/wakatime.cpp          # Updates WakaTime metrics
     ${CMAKE_CURRENT_LIST_DIR}/xpm.cpp               # All xpm files
 
-
-    ${CMAKE_CURRENT_LIST_DIR}/customprops/code_string_prop.cpp   # Derived wxStringProperty class for code
-    ${CMAKE_CURRENT_LIST_DIR}/customprops/custom_colour_prop.cpp # Property editor for colour
-    ${CMAKE_CURRENT_LIST_DIR}/customprops/custom_param_prop.cpp  # Derived wxStringProperty class for custom control parameters
-    ${CMAKE_CURRENT_LIST_DIR}/customprops/directory_prop.cpp     # Derived wxStringProperty class for choosing a directory
-    ${CMAKE_CURRENT_LIST_DIR}/customprops/evt_string_prop.cpp    # Derived wxStringProperty class for event function
-    ${CMAKE_CURRENT_LIST_DIR}/customprops/font_string_prop.cpp   # Derived wxStringProperty class for font property
+    ${CMAKE_CURRENT_LIST_DIR}/customprops/code_string_prop.cpp    # Derived wxStringProperty class for code
+    ${CMAKE_CURRENT_LIST_DIR}/customprops/custom_colour_prop.cpp  # Property editor for colour
+    ${CMAKE_CURRENT_LIST_DIR}/customprops/custom_param_prop.cpp   # Derived wxStringProperty class for custom control parameters
+    ${CMAKE_CURRENT_LIST_DIR}/customprops/directory_prop.cpp      # Derived wxStringProperty class for choosing a directory
+    ${CMAKE_CURRENT_LIST_DIR}/customprops/evt_string_prop.cpp     # Derived wxStringProperty class for event function
+    ${CMAKE_CURRENT_LIST_DIR}/customprops/font_string_prop.cpp    # Derived wxStringProperty class for font property
     ${CMAKE_CURRENT_LIST_DIR}/customprops/html_string_prop.cpp    # Derived wxStringProperty class for HTML
-    ${CMAKE_CURRENT_LIST_DIR}/customprops/img_string_prop.cpp    # Derived wxStringProperty class for handling wxImage files or art
-    ${CMAKE_CURRENT_LIST_DIR}/customprops/txt_string_prop.cpp    # Derived wxStringProperty class for single-line text
+    ${CMAKE_CURRENT_LIST_DIR}/customprops/img_string_prop.cpp     # Derived wxStringProperty class for handling wxImage files or art
+    ${CMAKE_CURRENT_LIST_DIR}/customprops/txt_string_prop.cpp     # Derived wxStringProperty class for single-line text
 
-    ${CMAKE_CURRENT_LIST_DIR}/customprops/art_prop_dlg.cpp      # Art Property Dialog for image property
-    ${CMAKE_CURRENT_LIST_DIR}/customprops/eventhandler_dlg.cpp  # Dialog for editing event handlers
-    ${CMAKE_CURRENT_LIST_DIR}/customprops/font_prop_dlg.cpp     # Dialog for editing Font Property
-    ${CMAKE_CURRENT_LIST_DIR}/customprops/img_props.cpp         # Handles property grid image properties
+    ${CMAKE_CURRENT_LIST_DIR}/customprops/art_prop_dlg.cpp        # Art Property Dialog for image property
+    ${CMAKE_CURRENT_LIST_DIR}/customprops/eventhandler_dlg.cpp    # Dialog for editing event handlers
+    ${CMAKE_CURRENT_LIST_DIR}/customprops/font_prop_dlg.cpp       # Dialog for editing Font Property
+    ${CMAKE_CURRENT_LIST_DIR}/customprops/img_props.cpp           # Handles property grid image properties
 
-    ${CMAKE_CURRENT_LIST_DIR}/customprops/pg_image.cpp       # Custom property grid class for images
-    ${CMAKE_CURRENT_LIST_DIR}/customprops/pg_animation.cpp   # Custom property grid class for animations
-    ${CMAKE_CURRENT_LIST_DIR}/customprops/pg_point.cpp       # Custom wxPGProperty for wxPoint
+    ${CMAKE_CURRENT_LIST_DIR}/customprops/pg_image.cpp            # Custom property grid class for images
+    ${CMAKE_CURRENT_LIST_DIR}/customprops/pg_animation.cpp        # Custom property grid class for animations
+    ${CMAKE_CURRENT_LIST_DIR}/customprops/pg_point.cpp            # Custom wxPGProperty for wxPoint
 
-    ${CMAKE_CURRENT_LIST_DIR}/custom_ctrls/colour_rect_ctrl.cpp  # Control that displays a solid color
+    ${CMAKE_CURRENT_LIST_DIR}/custom_ctrls/colour_rect_ctrl.cpp   # Control that displays a solid color
 
     ${CMAKE_CURRENT_LIST_DIR}/generate/gen_base.cpp          # Generate Src and Hdr files for Base and Derived Class
     ${CMAKE_CURRENT_LIST_DIR}/generate/gen_cmake.cpp         # Auto-generate a .cmake file
@@ -82,23 +81,23 @@ set (file_list
     ${CMAKE_CURRENT_LIST_DIR}/generate/window_widgets.cpp    # Splitter and Scroll generation classes
     ${CMAKE_CURRENT_LIST_DIR}/generate/wizard_form.cpp       # Wizard form class
 
-    ${CMAKE_CURRENT_LIST_DIR}/import/import_crafter_maps.cpp # wxCrafter mappings
-    ${CMAKE_CURRENT_LIST_DIR}/import/import_formblder.cpp    # Import a wxFormBuider project
-    ${CMAKE_CURRENT_LIST_DIR}/import/import_wxcrafter.cpp    # Import a wxCrafter project
-    ${CMAKE_CURRENT_LIST_DIR}/import/import_wxglade.cpp      # Import a Import a wxGlade file
-    ${CMAKE_CURRENT_LIST_DIR}/import/import_wxsmith.cpp      # Process XRC files
-    ${CMAKE_CURRENT_LIST_DIR}/import/import_xml.cpp          # Base class for XML importing
+    ${CMAKE_CURRENT_LIST_DIR}/import/import_crafter_maps.cpp  # wxCrafter mappings
+    ${CMAKE_CURRENT_LIST_DIR}/import/import_formblder.cpp     # Import a wxFormBuider project
+    ${CMAKE_CURRENT_LIST_DIR}/import/import_wxcrafter.cpp     # Import a wxCrafter project
+    ${CMAKE_CURRENT_LIST_DIR}/import/import_wxglade.cpp       # Import a Import a wxGlade file
+    ${CMAKE_CURRENT_LIST_DIR}/import/import_wxsmith.cpp       # Process XRC files
+    ${CMAKE_CURRENT_LIST_DIR}/import/import_xml.cpp           # Base class for XML importing
 
     ${CMAKE_CURRENT_LIST_DIR}/mockup/mockup_parent.cpp    # Top-level MockUp Parent window
     ${CMAKE_CURRENT_LIST_DIR}/mockup/mockup_content.cpp   # Mockup of a form's contents
     ${CMAKE_CURRENT_LIST_DIR}/mockup/mockup_wizard.cpp    # Emulate a wxWizard
 
-    ${CMAKE_CURRENT_LIST_DIR}/newdialogs/new_common.cpp    # Contains code common between all new_ dialogs
+    ${CMAKE_CURRENT_LIST_DIR}/newdialogs/new_common.cpp   # Contains code common between all new_ dialogs
     ${CMAKE_CURRENT_LIST_DIR}/newdialogs/new_dialog.cpp   # Dialog for creating a new project dialog
     ${CMAKE_CURRENT_LIST_DIR}/newdialogs/new_frame.cpp    # Dialog for creating a new project wxFrame
-    ${CMAKE_CURRENT_LIST_DIR}/newdialogs/new_panel.cpp     # Dialog for creating a new form panel
+    ${CMAKE_CURRENT_LIST_DIR}/newdialogs/new_panel.cpp    # Dialog for creating a new form panel
     ${CMAKE_CURRENT_LIST_DIR}/newdialogs/new_ribbon.cpp   # Dialog for creating a new wxRibbonBar
-    ${CMAKE_CURRENT_LIST_DIR}/newdialogs/new_wizard.cpp    # Dialog for creating a new wxWizard
+    ${CMAKE_CURRENT_LIST_DIR}/newdialogs/new_wizard.cpp   # Dialog for creating a new wxWizard
 
     ${CMAKE_CURRENT_LIST_DIR}/nodes/node.cpp              # Contains user-modifiable node
     ${CMAKE_CURRENT_LIST_DIR}/nodes/node_constants.cpp    # Maps wxWidgets constants to their numerical value
@@ -133,7 +132,7 @@ set (file_list
     ${CMAKE_CURRENT_LIST_DIR}/winres/winres_layout.cpp    # resForm layout code
     ${CMAKE_CURRENT_LIST_DIR}/winres/winres_styles.cpp    # resCtrl class style processing functions
 
-    pugixml/pugixml.cpp             # XML parser
+    ${CMAKE_CURRENT_LIST_DIR}/../pugixml/pugixml.cpp      # XML parser
 
     ${CMAKE_CURRENT_LIST_DIR}/ui/embedimg.cpp             # Convert image to Header (.h) or XPM (.xpm) file
     ${CMAKE_CURRENT_LIST_DIR}/ui/gridbag_item.cpp         # Dialog for inserting an item into a wxGridBagSizer node
@@ -144,14 +143,14 @@ set (file_list
 
     # ttLib submodule files
 
-    ${CMAKE_CURRENT_LIST_DIR}/../ttLib/src/ttcstr.cpp             # Class for handling zero-terminated char strings.
-    ${CMAKE_CURRENT_LIST_DIR}/../ttLib/src/ttcview.cpp            # string_view functionality on a zero-terminated char string.
-    ${CMAKE_CURRENT_LIST_DIR}/../ttLib/src/ttsview.cpp            # std::string_view with additional methods
-    ${CMAKE_CURRENT_LIST_DIR}/../ttLib/src/ttmultistr.cpp         # Breaks a single string into multiple strings
-    ${CMAKE_CURRENT_LIST_DIR}/../ttLib/src/ttparser.cpp           # Command line parser
-    ${CMAKE_CURRENT_LIST_DIR}/../ttLib/src/ttstr.cpp              # Enhanced version of wxString
-    ${CMAKE_CURRENT_LIST_DIR}/../ttLib/src/ttlibspace.cpp         # ttlib namespace functions
-    ${CMAKE_CURRENT_LIST_DIR}/../ttLib/src/tttextfile.cpp         # Classes for reading and writing text files.
+    ${CMAKE_CURRENT_LIST_DIR}/../ttLib/src/ttcstr.cpp      # Class for handling zero-terminated char strings.
+    ${CMAKE_CURRENT_LIST_DIR}/../ttLib/src/ttcview.cpp     # string_view functionality on a zero-terminated char string.
+    ${CMAKE_CURRENT_LIST_DIR}/../ttLib/src/ttsview.cpp     # std::string_view with additional methods
+    ${CMAKE_CURRENT_LIST_DIR}/../ttLib/src/ttmultistr.cpp  # Breaks a single string into multiple strings
+    ${CMAKE_CURRENT_LIST_DIR}/../ttLib/src/ttparser.cpp    # Command line parser
+    ${CMAKE_CURRENT_LIST_DIR}/../ttLib/src/ttstr.cpp       # Enhanced version of wxString
+    ${CMAKE_CURRENT_LIST_DIR}/../ttLib/src/ttlibspace.cpp  # ttlib namespace functions
+    ${CMAKE_CURRENT_LIST_DIR}/../ttLib/src/tttextfile.cpp  # Classes for reading and writing text files.
 
     # Debug-only files
 
@@ -161,10 +160,10 @@ set (file_list
     $<$<CONFIG:Debug>:${CMAKE_CURRENT_LIST_DIR}/debugging/msgframe.cpp>       # Stores messages
     $<$<CONFIG:Debug>:${CMAKE_CURRENT_LIST_DIR}/debugging/nodeinfo.cpp>       # Node memory usage dialog
 
-    $<$<CONFIG:Debug>:${CMAKE_CURRENT_LIST_DIR}/debugging/msgframe_base.cpp>  # wxUiEditor generated file
-    $<$<CONFIG:Debug>:${CMAKE_CURRENT_LIST_DIR}/debugging/nodeinfo_base.cpp>  # wxUiEditor generated file
-    $<$<CONFIG:Debug>:${CMAKE_CURRENT_LIST_DIR}/debugging/dbg_code_diff_base.cpp>
-    $<$<CONFIG:Debug>:${CMAKE_CURRENT_LIST_DIR}/debugging/debugsettingsBase.cpp>  # wxUiEditor generated file
+    $<$<CONFIG:Debug>:${CMAKE_CURRENT_LIST_DIR}/debugging/msgframe_base.cpp>       # wxUiEditor generated file
+    $<$<CONFIG:Debug>:${CMAKE_CURRENT_LIST_DIR}/debugging/nodeinfo_base.cpp>       # wxUiEditor generated file
+    $<$<CONFIG:Debug>:${CMAKE_CURRENT_LIST_DIR}/debugging/dbg_code_diff_base.cpp>  # wxUiEditor generated file
+    $<$<CONFIG:Debug>:${CMAKE_CURRENT_LIST_DIR}/debugging/debugsettingsBase.cpp>   # wxUiEditor generated file
 
     $<$<CONFIG:Debug>:${CMAKE_CURRENT_LIST_DIR}/../ttLib/src/winsrc/ttdebug_min.cpp>  # ttAssertionMsg
 

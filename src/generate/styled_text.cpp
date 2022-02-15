2d363b100c582eee64869d55022b285dc3a396a8
Randalphwa <38287198+Randalphwa@users.noreply.github.com>
2022-02-15 07:28:57 -0800

    Fix reversed boolean check for "no guides"
    I'm not sure why the previous fix prevented an empty parameter for
    SetIndentationGuides(), but the boolean check was reversed and prevented
    the SetIndentationGuides() from ever being called. Reversing it seems to work
    in the original test case as well with this fix.

--- src/generate/styled_text.cpp
+++ src/generate/styled_text.cpp
@@ -863,7 +863,7 @@ std::optional<ttlib::cstr> StyledTextGenerator::GenSettings(Node* node, size_t& 
 
     //////////// Tabs and Indentation settings ////////////
 
-    if (node->HasValue(prop_indentation_guides) && node->prop_as_string(prop_indentation_guides).is_sameas("no guides"))
+    if (node->HasValue(prop_indentation_guides) && !node->prop_as_string(prop_indentation_guides).is_sameas("no guides"))
     {
         code << "\n\t\t" << node->get_node_name() << "->SetIndentationGuides("
              << node->prop_as_constant(prop_indentation_guides, "stc_") << ");";

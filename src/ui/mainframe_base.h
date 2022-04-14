9a4d9b6e9f3da67cd51d91696e0a9f0e6ddbe68c
Randalphwa <38287198+Randalphwa@users.noreply.github.com>
2022-04-13 21:05:07 -0700

    Replace New toolbar/menu image
    This was also a TANGO image, but on Windows, it's difficult to see the grey
    image on the grey toolbar background. This switches it to a colored image.

--- src/ui/mainframe_base.h
+++ src/ui/mainframe_base.h
@@ -114,6 +114,7 @@ namespace wxue_img
     extern const unsigned char hidden_svg[2111];
     extern const unsigned char left_svg[585];
     extern const unsigned char magnify_svg[3953];
+    extern const unsigned char new_project_svg[921];
     extern const unsigned char right_svg[599];
     extern const unsigned char save_svg[717];
     extern const unsigned char top_svg[586];

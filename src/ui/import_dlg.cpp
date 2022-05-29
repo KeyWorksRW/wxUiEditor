565f962cd69b30a337b792a735fc19847a01ec48
Randalphwa <38287198+Randalphwa@users.noreply.github.com>
2022-05-28 19:45:38 -0700

    Add missing header needed for release builds

--- src/ui/import_dlg.cpp
+++ src/ui/import_dlg.cpp
@@ -5,14 +5,11 @@
 // License:   Apache License -- see ../../LICENSE
 /////////////////////////////////////////////////////////////////////////////
 
-#include <wx/config.h>   // wxConfig base header
-#include <wx/dir.h>      // wxDir is a class for enumerating the files in a directory
-#include <wx/dirdlg.h>   // wxDirDialog base class
-#include <wx/filedlg.h>  // wxFileDialog base header
-
-#if defined(_DEBUG)
-    #include <wx/filename.h>
-#endif
+#include <wx/config.h>    // wxConfig base header
+#include <wx/dir.h>       // wxDir is a class for enumerating the files in a directory
+#include <wx/dirdlg.h>    // wxDirDialog base class
+#include <wx/filedlg.h>   // wxFileDialog base header
+#include <wx/filename.h>  // wxFileName - encapsulates a file path
 
 #include "pugixml.hpp"   // xml processing
 #include "tttextfile.h"  // textfile -- Classes for reading and writing line-oriented files

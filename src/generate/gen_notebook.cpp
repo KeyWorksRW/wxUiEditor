6dd9ae0f7bd6da5438b30f9d2adec727db3993cd
Randalphwa <38287198+Randalphwa@users.noreply.github.com>
2022-06-03 07:30:13 -0700

    Add XRC generator for wxNoteBook

--- src/generate/gen_notebook.cpp
+++ src/generate/gen_notebook.cpp
@@ -67,3 +67,37 @@ bool NotebookGenerator::GetIncludes(Node* node, std::set<std::string>& set_src, 
 
     return true;
 }
+
+// ../../wxSnapShot/src/xrc/xh_notbk.cpp
+// ../../../wxWidgets/src/xrc/xh_notbk.cpp
+
+int NotebookGenerator::GenXrcObject(Node* node, pugi::xml_node& object, bool add_comments)
+{
+    auto result = node->GetParent()->IsSizer() ? BaseGenerator::xrc_sizer_item_created : BaseGenerator::xrc_updated;
+    auto item = InitializeXrcObject(node, object);
+
+    GenXrcObjectAttributes(node, item, "wxNotebook");
+
+    ttlib::cstr styles(node->prop_as_string(prop_style));
+    if (node->prop_as_string(prop_tab_position) != "wxBK_DEFAULT")
+    {
+        if (styles.size())
+            styles << '|';
+        styles << node->prop_as_string(prop_tab_position);
+    }
+
+    GenXrcPreStylePosSize(node, item, styles);
+    GenXrcWindowSettings(node, item);
+
+    if (add_comments)
+    {
+        GenXrcComments(node, item);
+    }
+
+    return result;
+}
+
+void NotebookGenerator::RequiredHandlers(Node* /* node */, std::set<std::string>& handlers)
+{
+    handlers.emplace("wxNotebookXmlHandler");
+}

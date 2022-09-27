a857c3d3d33aa9c484b687d7112f9f940992959f
Randalphwa <38287198+Randalphwa@users.noreply.github.com>
2022-09-27 12:22:34 -0700

    Implement the function to do the actual testing
    This creates the XRC code as an XML document, then imports it to create
    a temporary node tree. It relies on the various MSG_() macros in the importer
    to report problems in the message window.

--- src/tests/test_xrc_import.cpp
+++ src/tests/test_xrc_import.cpp
@@ -9,12 +9,86 @@
 // does is create an XML document containing the XRC for the selected node, then imports that
 // document letting the standard MSG_INFO messages to let us know if there were problems.
 
-#include "mainframe.h"      // MainFrame -- Main window frame
-#include "node.h"           // Node class
+#include "../import/import_wxsmith.h"  // Import a wxSmith file
+#include "gen_xrc.h"                   // Generate XRC file
+#include "mainframe.h"                 // MainFrame -- Main window frame
+#include "node.h"                      // Node class
+#include "project_class.h"             // Project class
+
+#include "pugixml.hpp"
+
+static void CountNodes(Node* node, size_t& nodes)
+{
+    ++nodes;
+    for (auto& iter: node->GetChildNodePtrs())
+    {
+        CountNodes(iter.get(), nodes);
+    }
+}
 
 void MainFrame::OnTestXrcImport(wxCommandEvent& /* event */)
 {
-    ASSERT_MSG(m_selected_node, "No node selected");
+    if (!m_selected_node)
+    {
+        wxMessageBox("You need to select a form first.", "Compare");
+        return;
+    }
 
+    auto form_node = m_selected_node.get();
+    if (!form_node->IsForm())
+    {
+        if (!form_node->isGen(gen_Project))
+        {
+            form_node = form_node->get_form();
+        }
+    }
 
+    pugi::xml_document doc;
+    {
+        // Place this in a block so that the string is destroyed before we process the XML
+        // document (to save allocated memory).
+        auto doc_str = GenerateXrcStr(form_node, xrc::no_flags);
+        auto result = doc.load_string(doc_str.c_str());
+        if (!result)
+        {
+            wxMessageBox("Error parsing XRC document: " + ttlib::cstr(result.description()), "XRC Import Test");
+            return;
+        }
+    }
+
+    auto root = doc.first_child();
+    if (!ttlib::is_sameas(root.name(), "resource", tt::CASE::either))
+    {
+        wxMessageBox("Invalid XRC -- no resource object", "Import XRC Test");
+        return;
+    }
+
+    MSG_INFO("--- Importing XRC document ---");
+
+    WxSmith doc_import;
+
+    size_t original_node_count = 0;
+    CountNodes(form_node, original_node_count);
+    size_t new_node_count = 0;
+    // If this is an actual form rather than the project, then there will only be one child
+    // object, which is the form.
+    for (auto& iter: root.children())
+    {
+        auto new_node = doc_import.CreateXrcNode(iter, nullptr);
+        if (new_node)
+        {
+            CountNodes(new_node.get(), new_node_count);
+        }
+    }
+
+    if (original_node_count != new_node_count)
+    {
+        // This isn't always a problem, but it's an indication that something might be wrong.
+        MSG_INFO(ttlib::cstr("Original node count: ") << original_node_count);
+        MSG_INFO(ttlib::cstr("New node count: ") << new_node_count);
+    }
+    else
+    {
+        MSG_INFO("Node counts match");
+    }
 }

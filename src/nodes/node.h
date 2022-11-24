cdb40882425693a1be5bc29b20dc4a3b6f869e66
Randalphwa <38287198+Randalphwa@users.noreply.github.com>
2022-11-24 13:32:35 -0800

    Add multi-language version of get_node_name
    Besides adding a '$' prefix for PHP, we may at some point want to remove
    the "m_" prefix from non C++ generated code names.

--- src/nodes/node.h
+++ src/nodes/node.h
@@ -128,6 +128,8 @@ public:
     // Returns the value of the property "var_name" or "class_name"
     const ttlib::cstr& get_node_name() const;
 
+    ttlib::cstr get_node_name(int language) const;
+
     // Returns the value of the parent property "var_name" or "class_name"
     const ttlib::cstr& get_parent_name() const;
 

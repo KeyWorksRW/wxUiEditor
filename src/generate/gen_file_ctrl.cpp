80b3a3e5d52f82856d12017bb54929347164ecca
Randalphwa <38287198+Randalphwa@users.noreply.github.com>
2022-12-21 06:15:10 -0800

    Add Python support for wxFileCtrl, fix bugs
    Both Mockup and C++ code generation now support prop_filter_index.
    
    prop_initial_folder is now set to type_path, and prop_initial_filename is
    now set to type_file.
    
    Changing a wildcard property now re-creates the entire Mockup panel
    (required for wxFileCtrl to accurately display the new wildcard pattern).

--- src/generate/gen_file_ctrl.cpp
+++ src/generate/gen_file_ctrl.cpp
@@ -30,74 +30,64 @@ wxObject* FileCtrlGenerator::CreateMockup(Node* node, wxObject* parent)
     if (!(node->prop_as_int(prop_style) & wxFC_NOSHOWHIDDEN))
         widget->ShowHidden(node->prop_as_bool(prop_show_hidden));
 
+    if (node->prop_as_int(prop_filter_index) > 0)
+        widget->SetFilterIndex(node->prop_as_int(prop_filter_index));
+
     widget->Bind(wxEVT_LEFT_DOWN, &BaseGenerator::OnLeftClick, this);
 
     return widget;
 }
 
-std::optional<ttlib::cstr> FileCtrlGenerator::GenConstruction(Node* node)
+std::optional<ttlib::sview> FileCtrlGenerator::CommonConstruction(Code& code)
 {
-    ttlib::cstr code;
-    code << "\t";
-    if (node->IsLocal())
+    if (code.is_cpp() && code.is_local_var())
         code << "auto* ";
-    code << node->get_node_name() << GenerateNewAssignment(node);
-    code << GetParentName(node) << ", " << node->prop_as_string(prop_id) << ", ";
-
-    if (node->HasValue(prop_initial_folder))
-        code << GenerateQuotedString(node->prop_as_string(prop_initial_folder));
+    code.NodeName().CreateClass();
+    code.GetParentName().Comma().as_string(prop_id);
+    code.Comma().QuotedString(prop_initial_folder).Comma().QuotedString(prop_initial_filename);
+    code.Comma();
+    if (code.HasValue(prop_wildcard))
+        code.QuotedString(prop_wildcard);
     else
-        code << "wxEmptyString";
-    code << ", ";
+        code.Add("wxFileSelectorDefaultWildcardStr");
 
-    if (node->HasValue(prop_initial_filename))
-        code << GenerateQuotedString(node->prop_as_string(prop_initial_filename));
-    else
-        code << "wxEmptyString";
-    code << ", ";
+    // Unlike most controls, wxFileCtrl expects the style to be specified *before* the
+    // position and size.
 
-    if (node->HasValue(prop_wildcard))
-        code << GenerateQuotedString(node->prop_as_string(prop_wildcard));
-    else
-        code << "wxFileSelectorDefaultWildcardStr";
-    code << ", ";
-
-    GenStyle(node, code);
-
-    code << ",\n\t\t\t";
-    GenPos(node, code);
-    code << ", ";
-    GenSize(node, code);
-    if (node->HasValue(prop_window_name))
+    auto what_params = code.WhatParamsNeeded("wxFC_DEFAULT_STYLE");
+    if (what_params != Code::nothing_needed)
     {
-        code << ", " << node->prop_as_string(prop_window_name);
+        code.Comma().CheckLineLength(prop_style).Style();
+        if (what_params != Code::style_needed)
+        {
+            code.Comma().CheckLineLength().Pos().Comma().CheckLineLength().WxSize();
+            if (what_params & Code::window_name_needed)
+                code.Comma().QuotedString(prop_window_name);
+        }
     }
-    code << ");";
+    code.EndFunction();
 
-    if (node->prop_as_bool(prop_show_hidden))
-    {
-        code << "\n" << node->get_node_name() << "->ShowHidden(true);";
-    }
-
-    return code;
+    return code.m_code;
 }
 
-std::optional<ttlib::cstr> FileCtrlGenerator::GenSettings(Node* node, size_t& /* auto_indent */)
+std::optional<ttlib::sview> FileCtrlGenerator::CommonSettings(Code& code)
 {
-    ttlib::cstr code;
-
-    if (node->prop_as_bool(prop_focus))
+    if (code.IsTrue(prop_focus))
     {
-        if (code.size())
-            code << '\n';
-        code << node->get_node_name() << "->SetFocus()";
+        code.Eol(true).NodeName().Function("SetFocus(").EndFunction();
     }
 
-    if (code.size())
-        return code;
-    else
+    if (code.IntValue(prop_filter_index) > 0)
+    {
+        code.Eol(true).NodeName().Function("SetFilterIndex(").Str(prop_filter_index).EndFunction();
+    }
 
-        return {};
+    if (code.IsTrue(prop_show_hidden))
+    {
+        code.Eol(true).NodeName().Function("ShowHidden(").AddTrue().EndFunction();
+    }
+
+    return code.m_code;
 }
 
 bool FileCtrlGenerator::GetIncludes(Node* node, std::set<std::string>& set_src, std::set<std::string>& set_hdr)

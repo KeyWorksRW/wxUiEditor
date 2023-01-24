9d756a161070446feaf65400dfb6925ed933bc45
Randalphwa <38287198+Randalphwa@users.noreply.github.com>
2023-01-24 09:56:44 -0800

    Move lambdas function into header file
    It's so small, there's no upside to having it in it's own src file.

--- src/lambdas.h
+++ src/lambdas.h
@@ -1,9 +1,18 @@
 /////////////////////////////////////////////////////////////////////////////
 // Purpose:   Functions for formatting and storage of lamda events
 // Author:    Ralph Walden
-// Copyright: Copyright (c) 2020-2021 KeyWorks Software (Ralph Walden)
+// Copyright: Copyright (c) 2020-2023 KeyWorks Software (Ralph Walden)
 // License:   Apache License -- see ../../LICENSE
 /////////////////////////////////////////////////////////////////////////////
 
 // Replaces @@ with \n
-void ExpandLambda(ttlib::cstr& lambda);
+inline void ExpandLambda(ttlib::cstr& lambda)
+{
+    lambda.LeftTrim();
+    if (lambda.starts_with("@@"))
+    {
+        lambda.erase(0, 2);
+    }
+    lambda.Replace("@@", "\n", tt::REPLACE::all);
+    lambda.RightTrim();
+}

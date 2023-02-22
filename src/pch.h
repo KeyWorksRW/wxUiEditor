30eec4ce4ca44b450dc6f036d6090661c0ceae1c
Randalphwa <38287198+Randalphwa@users.noreply.github.com>
2023-02-22 05:23:02 -0800

    Change project version to 16
    The commit that changed this to 17 was wrong! This version should only be
    changed for a release, and there was no official release before 1.1.0.0.

--- src/pch.h
+++ src/pch.h
@@ -137,11 +137,14 @@ constexpr const char* txtAppname = "wxUiEditor";
 // This is the highest project number supported by this build of wxUiEditor. It should be
 // updated after every release, if there are any changes to the project format that might
 // require a newer version.
-constexpr const int curSupportedVer = 17;
+constexpr const int curSupportedVer = 16;
 
 // This is the default minimum required version for all generators. It is the version used by
 // the 1.0.0 release.
 constexpr const int minRequiredVer = 15;
+
+// 1.0 == version 15
+// 1.1 == version 16
 
 // Use when you need to return an empty const tt_string&
 extern tt_string tt_empty_cstr;

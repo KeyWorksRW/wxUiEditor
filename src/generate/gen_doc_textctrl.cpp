3ca5e6747b44fd12cfeee59a4c225e1047c810b8
Randalphwa <38287198+Randalphwa@users.noreply.github.com>
2023-05-01 08:37:49 -0700

    Rework document/view generators
    These are now forms rather than children.

--- src/generate/gen_doc_textctrl.cpp
+++ src/generate/gen_doc_textctrl.cpp
@@ -10,7 +10,7 @@
 #include "code.h"  // Code -- Helper class for generating code
 
 inline constexpr const auto txt_TextCtrlViewBlock =
-    R"===(wxIMPLEMENT_DYNAMIC_CLASS(%class%, wxDocument);
+R"===(wxIMPLEMENT_DYNAMIC_CLASS(%class%, wxDocument);
 
 bool %class%::OnCreate(const wxString& path, long flags)
 {
@@ -82,13 +82,22 @@ bool TextDocumentGenerator::ConstructionCode(Code& code)
     {
         tt_string_vector lines;
         lines.ReadString(txt_TextCtrlViewBlock);
-        tt_string class_name = code.node()->value(prop_class_name);
+        tt_string class_name = code.node()->GetParent()->value(prop_class_name);
         for (auto& line: lines)
         {
             line.Replace("%class%", class_name, true);
             code.Str(line).Eol();
         }
     }
+
+    return true;
+}
+
+bool TextDocumentGenerator::GetIncludes(Node* /* node */, std::set<std::string>& set_src, std::set<std::string>& /* set_hdr */)
+{
+    set_src.insert("#include <wx/docmdi.h");
+    set_src.insert("#include <wx/docview.h");
+    set_src.insert("#include <wx/textctrl.h");
 
     return true;
 }

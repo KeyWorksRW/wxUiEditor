5ce4cc5ab20a490c227faa20b5350c2db3de1734
Randalphwa <38287198+Randalphwa@users.noreply.github.com>
2023-06-26 15:12:17 -0700

    Regenerate .cmake file
    Regenerated now the classes are divided into two lists.

--- tests/sdi/cpp/wxui_code.cmake
+++ tests/sdi/cpp/wxui_code.cmake
@@ -4,17 +4,20 @@
 
 set (sdi_generated_code
 
+    # Non-base classes
     ${CMAKE_CURRENT_LIST_DIR}/booktest_dlg.cpp
     ${CMAKE_CURRENT_LIST_DIR}/commonctrls.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/dlgissue_956.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/dlgissue_960.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/form_toolbar_base.cpp
     ${CMAKE_CURRENT_LIST_DIR}/images.cpp
     ${CMAKE_CURRENT_LIST_DIR}/mainframe.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/maintestdialog.cpp
     ${CMAKE_CURRENT_LIST_DIR}/popupwin.cpp
     ${CMAKE_CURRENT_LIST_DIR}/python_dlg.cpp
     ${CMAKE_CURRENT_LIST_DIR}/testformpanel.cpp
     ${CMAKE_CURRENT_LIST_DIR}/wizard.cpp
+
+    # Base classes
+    ${CMAKE_CURRENT_LIST_DIR}/dlgissue_956.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/dlgissue_960.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/form_toolbar_base.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/maintestdialog.cpp
 
 )

393f65390f2ded9a4c6c374bb2cf679265f81a62
Randalphwa <38287198+Randalphwa@users.noreply.github.com>
2023-07-10 15:44:47 -0700

    Add GetOldNode() override to ChangeType classes

--- src/undo_cmds.h
+++ src/undo_cmds.h
@@ -191,7 +191,7 @@ public:
     void Change() override;
     void Revert() override;
 
-    Node* GetOldNode() { return m_old_node.get(); }
+    NodeSharedPtr GetOldNode() override { return m_old_node; }
     Node* GetNode() { return m_node.get(); }
 
     size_t GetMemorySize() override { return sizeof(*this); }
@@ -210,7 +210,7 @@ public:
     void Change() override;
     void Revert() override;
 
-    Node* GetOldNode() { return m_old_node.get(); }
+    NodeSharedPtr GetOldNode() override { return m_old_node; }
     Node* GetNode() { return m_node.get(); }
 
     size_t GetMemorySize() override { return sizeof(*this); }
@@ -276,11 +276,13 @@ public:
     void Revert() override;
 
     size_t GetMemorySize() override { return sizeof(*this); }
+    NodeSharedPtr GetOldNode() override { return m_old_project; }
 
 protected:
     void SortFolder(Node* folder);
 
 private:
+    std::vector<NodeSharedPtr> m_children;
     NodeSharedPtr m_old_project;
 };
 

23ffe10ae02d59513107106c1e0c04fe3ec7fa8e
Randalphwa <38287198+Randalphwa@users.noreply.github.com>
2023-08-01 18:39:10 -0700

    Initialize new project from UserPrefs
    Sets line length for all languages, and wxWidgets version for C++

--- src/project/loadproject.cpp
+++ src/project/loadproject.cpp
@@ -15,6 +15,7 @@
 #include "node.h"             // Node class
 #include "node_creator.h"     // NodeCreator class
 #include "project_handler.h"  // ProjectHandler class
+#include "preferences.h"      // Prefs -- Set/Get wxUiEditor preferences
 
 using namespace GenEnum;
 
@@ -903,6 +904,11 @@ bool ProjectHandler::NewProject(bool create_empty, bool allow_ui)
                 }
             }
         }
+
+        project->set_value(prop_cpp_line_length, UserPrefs.get_CppLineLength());
+        project->set_value(prop_python_line_length, UserPrefs.get_PythonLineLength());
+        project->set_value(prop_ruby_line_length, UserPrefs.get_RubyLineLength());
+        project->set_value(prop_wxWidgets_version, UserPrefs.get_CppWidgetsVersion());
 
         // Calling this will also initialize the ProjectImage class
         Project.Initialize(project);

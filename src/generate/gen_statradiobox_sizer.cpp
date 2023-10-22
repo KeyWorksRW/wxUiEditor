01357fc5410ecd3d5f0d6dae6de47ac80cea91a2
Randalphwa <38287198+Randalphwa@users.noreply.github.com>
2023-10-22 13:20:09 -0700

    wxRuby: remove bogus message
    It *IS* valid to use a radio button as a label for wxStaticBoxSizer in
    wxRuby3

--- src/generate/gen_statradiobox_sizer.cpp
+++ src/generate/gen_statradiobox_sizer.cpp
@@ -295,15 +295,8 @@ std::optional<tt_string> StaticRadioBtnBoxSizerGenerator::GetWarning(Node* node,
             }
 
         case GEN_LANG_RUBY:
-            {
-                tt_string msg;
-                if (auto form = node->getForm(); form && form->hasValue(prop_class_name))
-                {
-                    msg << form->as_string(prop_class_name) << ": ";
-                }
-                msg << "wxRuby currently does not support a radio button as a static box label";
-                return msg;
-            }
+            return {};
+
         default:
             return {};
     }

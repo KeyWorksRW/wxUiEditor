480c644a2c1999ea650e397746d8a12b526745d2
Randalphwa <38287198+Randalphwa@users.noreply.github.com>
2023-11-10 12:20:07 -0800

    Fix clang-format

--- src/panels/doc_view.h
+++ src/panels/doc_view.h
@@ -65,8 +65,8 @@ private:
 // Code below this comment block will be preserved
 // if the code for this class is re-generated.
 //
-// clang-format on
-// ***********************************************
+    // clang-format on
+    // ***********************************************
 
 public:
     DocViewPanel(wxWindow* parent, MainFrame* frame);

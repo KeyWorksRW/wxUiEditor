831b68edda964644bf3f5f59ff3383c637c0a7ab
Randalphwa <38287198+Randalphwa@users.noreply.github.com>
2023-11-20 16:04:58 -0800

    Combine nested conditionals

--- src/mainapp.cpp
+++ src/mainapp.cpp
@@ -31,22 +31,19 @@
 
 #include "pugixml.hpp"
 
-#ifdef _MSC_VER
-    #if defined(_WIN32)
+#if defined(_WIN32) && defined(_MSC_VER)
+    #pragma comment(lib, "kernel32.lib")
+    #pragma comment(lib, "user32.lib")
+    #pragma comment(lib, "gdi32.lib")
+    #pragma comment(lib, "comctl32.lib")
+    #pragma comment(lib, "comdlg32.lib")
+    #pragma comment(lib, "shell32.lib")
 
-        #pragma comment(lib, "kernel32.lib")
-        #pragma comment(lib, "user32.lib")
-        #pragma comment(lib, "gdi32.lib")
-        #pragma comment(lib, "comctl32.lib")
-        #pragma comment(lib, "comdlg32.lib")
-        #pragma comment(lib, "shell32.lib")
+    #pragma comment(lib, "rpcrt4.lib")
+    #pragma comment(lib, "advapi32.lib")
 
-        #pragma comment(lib, "rpcrt4.lib")
-        #pragma comment(lib, "advapi32.lib")
-
-        #if wxUSE_URL_NATIVE
-            #pragma comment(lib, "wininet.lib")
-        #endif
+    #if wxUSE_URL_NATIVE
+        #pragma comment(lib, "wininet.lib")
     #endif
 #endif
 
@@ -559,7 +556,7 @@ protected:
 
 void App::OnFatalException()
 {
-    #if defined(_DEBUG) &&  defined(wxUSE_STACKWALKER)
+    #if defined(_DEBUG) && defined(wxUSE_STACKWALKER)
 
     StackLogger logger;
     logger.WalkFromException();

955154abe0a929ec1ce19beb96cac0daa2dd5b6c
Randalphwa <38287198+Randalphwa@users.noreply.github.com>
2024-10-01 12:45:13 -0700

    Placeholder dlg construction code for new languages

--- src/generate/gen_dialog.cpp
+++ src/generate/gen_dialog.cpp
@@ -148,6 +148,35 @@ bool DialogFormGenerator::ConstructionCode(Code& code)
             code.GetCode().Replace("\t\t\t\t", spaces, true);
         }
     }
+    else if (code.is_haskell())
+    {
+        code.Str("instance ").NodeName().Str("Class where");
+        code.Eol().Str("create parent id title pos size style name = do");
+        code.Indent();
+        code.Eol().Str("dialog <- wxDialogCreate parent id title pos size style name");
+    }
+    else if (code.is_lua())
+    {
+        code.Eol().NodeName().Str(" = wx.wxDialog(parent, id, title, pos, size, style, name)");
+    }
+    else if (code.is_perl())
+    {
+        code.Str("sub new {");
+        code.Indent();
+        code.Eol().Str("my( $class, $parent, $id, $title, $pos, $size, $style, $name ) = @_;");
+        code.Eol().Str("my $this = $class->SUPER::new( $parent, $id, $title, $pos, $size, $style, $name );");
+    }
+    else if (code.is_php())
+    {
+        code.Str("class ").NodeName().Str(" extends wxDialog");
+        code.Eol().Str("{");
+        code.Indent();
+        code.Eol().Str("public function __construct()");
+        code.Eol().Str("{");
+        code.Indent();
+        code.Eol().Str("parent::__construct($parent, $id, $title, $pos, $size, $style, $name);");
+    }
+
     else
     {
         code.AddComment("Unknown language", true);

da1eef3822be215c54ee751a3cbfed7d45b35b01
Randalphwa <38287198+Randalphwa@users.noreply.github.com>
2025-01-25 20:53:11 -0800

    Switch to using wxFileName()

--- src/customprops/directory_prop.cpp
+++ src/customprops/directory_prop.cpp
@@ -1,7 +1,7 @@
 /////////////////////////////////////////////////////////////////////////////
 // Purpose:   Derived wxStringProperty class for choosing a directory
 // Author:    Ralph Walden
-// Copyright: Copyright (c) 2021-2023 KeyWorks Software (Ralph Walden)
+// Copyright: Copyright (c) 2021-2025 KeyWorks Software (Ralph Walden)
 // License:   Apache License -- see ../../LICENSE
 /////////////////////////////////////////////////////////////////////////////
 
@@ -34,24 +34,43 @@ bool DirectoryDialogAdapter::DoShowDialog(wxPropertyGrid* propGrid, wxPGProperty
         dlg_pos = propGrid->GetGoodEditorDialogPosition(property, dlg_sz);
     }
 
-    tt_string path = Project.getProjectPath();
+    wxFileName path;
     auto node = m_prop->getNode();
     if (node->isGen(gen_wxFilePickerCtrl))
     {
-        path = m_prop->as_string().size() ? m_prop->as_string() : Project.getProjectPath();
+        if (m_prop->as_string().size())
+        {
+            path.AssignDir(m_prop->as_string());
+        }
+        else
+        {
+            path = *Project.get_wxFileName();
+            path.SetFullName(wxEmptyString);  // clear the project filename
+        }
     }
     else
     {
-        path.append_filename(m_prop->as_string());
-        path.make_absolute();
+        path = *Project.get_wxFileName();
+        path.SetFullName(wxEmptyString);  // clear the project filename
+        if (m_prop->as_string().size())
+        {
+            wxFileName prop_path;
+            prop_path.AssignDir(m_prop->as_string());
+            for (auto& iter: prop_path.GetDirs())
+            {
+                path.AppendDir(iter);
+            }
+            path.MakeAbsolute();
+        }
     }
 
     // If the directory doesn't exist, then we need to reset it. Otherwise on Windows, the
     // dialog will be for the computer, requiring the user to drill down to where the project
     // file is.
-    if (!node->isGen(gen_wxFilePickerCtrl) && !path.dir_exists())
+    if (!node->isGen(gen_wxFilePickerCtrl) && !path.DirExists())
     {
-        path = Project.getProjectPath();
+        path = *Project.get_wxFileName();
+        path.SetFullName(wxEmptyString);  // clear the project filename
     }
 
     auto style = wxDD_DEFAULT_STYLE | wxDD_CHANGE_DIR;
@@ -60,7 +79,7 @@ bool DirectoryDialogAdapter::DoShowDialog(wxPropertyGrid* propGrid, wxPGProperty
         style |= wxDD_DIR_MUST_EXIST;
     }
 
-    wxDirDialog dlg(propGrid, wxDirSelectorPromptStr, path.make_wxString(), style, dlg_pos, dlg_sz);
+    wxDirDialog dlg(propGrid, wxDirSelectorPromptStr, path.GetPath(), style, dlg_pos, dlg_sz);
     if (dlg.ShowModal() == wxID_OK)
     {
         SetValue(dlg.GetPath());

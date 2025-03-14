c9e9b7141e57ad14f00ebd02cd643828f3c7c17c
Randalphwa <38287198+Randalphwa@users.noreply.github.com>
2025-03-14 11:35:43 -0700

    Restore Single Code line editor
    This was mistakenly overwritten by the custom control parameter dialog.

---
+++ src/customprops/code_single_prop.cpp
@@ -0,0 +1,72 @@
+/////////////////////////////////////////////////////////////////////////////
+// Purpose:   Derived wxStringProperty class for single line code
+// Author:    Ralph Walden
+// Copyright: Copyright (c) 2021-2023 KeyWorks Software (Ralph Walden)
+// License:   Apache License -- see ../../LICENSE
+/////////////////////////////////////////////////////////////////////////////
+
+#include <wx/propgrid/propgrid.h>  // wxPropertyGrid
+
+#include "code_single_prop.h"
+
+#include "../nodes/node.h"       // Node class
+#include "../nodes/node_prop.h"  // NodeProperty class
+
+#include "wxui/editstringdialog_base.h"  // auto-generated: wxui/editstringdialog_base.h wxui/editstringdialog_base.cpp
+
+EditCodeSingleProperty::EditCodeSingleProperty(const wxString& label, NodeProperty* prop) :
+    wxStringProperty(label, wxPG_LABEL, prop->as_wxString()), m_prop(prop)
+{
+}
+
+class EditCodeSingleDialog : public EditStringDialogBase
+{
+public:
+    EditCodeSingleDialog(wxWindow* parent, NodeProperty* prop) : EditStringDialogBase(parent)
+    {
+        SetTitle(tt_string() << prop->declName() << " property editor");
+        m_value = prop->as_wxString();
+        m_static_hdr_text->Show();
+        m_node = prop->getNode();
+        m_prop = prop;
+
+        m_textCtrl->Bind(wxEVT_TEXT, &EditCodeSingleDialog::UpdateStaticText, this);
+        Fit();
+    };
+
+    void UpdateStaticText(wxCommandEvent& /* event */)
+    {
+        tt_string static_text;
+        if (m_prop->isProp(prop_cpp_conditional))
+        {
+            auto text = m_textCtrl->GetValue().utf8_string();
+            if (!text.starts_with("#"))
+                static_text << "#if ";
+            static_text << m_textCtrl->GetValue().utf8_string();
+        }
+        else
+        {
+            if (m_node->isPropValue(prop_class_access, "none"))
+                static_text << "auto ";
+            static_text << m_node->as_string(prop_var_name) << " = new " << m_node->as_string(prop_class_name);
+            static_text << m_textCtrl->GetValue().utf8_string() << ';';
+        }
+        m_static_hdr_text->SetLabel(static_text.make_wxString());
+    }
+
+private:
+    Node* m_node;
+    NodeProperty* m_prop;
+};
+
+bool EditCodeSingleDialogAdapter::DoShowDialog(wxPropertyGrid* propGrid, wxPGProperty* WXUNUSED(property))
+{
+    EditCodeSingleDialog dlg(propGrid->GetPanel(), m_prop);
+    if (dlg.ShowModal() == wxID_OK)
+    {
+        SetValue(dlg.GetResults());
+        return true;
+    }
+
+    return false;
+}
---
+++ src/customprops/code_single_prop.h
@@ -0,0 +1,39 @@
+/////////////////////////////////////////////////////////////////////////////
+// Purpose:   Derived wxStringProperty class for single line code
+// Author:    Ralph Walden
+// Copyright: Copyright (c) 2021 KeyWorks Software (Ralph Walden)
+// License:   Apache License -- see ../../LICENSE
+/////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include <wx/propgrid/editors.h>  // wxPropertyGrid editors
+#include <wx/propgrid/props.h>    // wxPropertyGrid Property Classes
+
+class NodeProperty;
+
+class EditCodeSingleDialogAdapter : public wxPGEditorDialogAdapter
+{
+public:
+    EditCodeSingleDialogAdapter(NodeProperty* prop) : wxPGEditorDialogAdapter(), m_prop(prop) {}
+
+    bool DoShowDialog(wxPropertyGrid* WXUNUSED(propGrid), wxPGProperty* WXUNUSED(property)) override;
+
+private:
+    NodeProperty* m_prop;
+};
+
+class EditCodeSingleProperty : public wxStringProperty
+{
+public:
+    EditCodeSingleProperty(const wxString& label, NodeProperty* prop);
+
+    // Set editor to have button
+    const wxPGEditor* DoGetEditorClass() const override { return wxPGEditor_TextCtrlAndButton; }
+
+    // Set what happens on button click
+    wxPGEditorDialogAdapter* GetEditorDialog() const override { return new EditCodeSingleDialogAdapter(m_prop); }
+
+private:
+    NodeProperty* m_prop;
+};

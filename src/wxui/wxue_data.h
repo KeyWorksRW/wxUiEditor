54e90142efe1ef6a7b45d489ae88c34d17cb4900
Randalphwa <38287198+Randalphwa@users.noreply.github.com>
2025-10-08 18:36:41 -0700

    Add minimum_compiler and trailing_return_type
    These are for C++ code generation

--- src/wxui/wxue_data.h
+++ src/wxui/wxue_data.h
@@ -46,7 +46,7 @@ namespace wxue_data
     // ../xml/forms.xml
     extern const unsigned char forms[6633]; // Original size: 55,980 bytes
     // ../xml/interfaces.xml
-    extern const unsigned char interfaces[10837]; // Original size: 52,122 bytes
+    extern const unsigned char interfaces[10943]; // Original size: 52,516 bytes
     // ../xml/mdi.xml
     extern const unsigned char mdi[1995]; // Original size: 17,170 bytes
     // ../xml/pickers.xml

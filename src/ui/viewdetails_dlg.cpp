b9ac076abc0cef64c3700174b8c0849c3e70ca34
Randalphwa <38287198+Randalphwa@users.noreply.github.com>
2026-06-08 12:26:31 -0700

    build issues (#1800)
    * Fix broken paths
    
    When wxue created the derived files, it didn't change the header file to be relative to where the derived file was created, creating a full path instead.
    
    * Prevent tracking deno.jsonc and any files with a '.old' extension

--- src/ui/viewdetails_dlg.cpp
+++ src/ui/viewdetails_dlg.cpp
@@ -1,32 +1,21 @@
 /////////////////////////////////////////////////////////////////////////////
-// Purpose:   ViewDetails dialog class
+// Purpose:   ViewDetails (node details) dialog class
 // Author:    Ralph Walden
 // Copyright: Copyright (c) 2026 KeyWorks Software (Ralph Walden)
 // License:   Apache License -- see ..\..\LICENSE
 /////////////////////////////////////////////////////////////////////////////
 
-#include "viewdetails_dlg.h"  // auto-generated: C:/rwCode/wxLanguages/wxUiEditor2/src/wxui/viewdetails_base.h and C:/rwCode/wxLanguages/wxUiEditor2/src/wxui/viewdetails_base.cpp
+#include "viewdetails_dlg.h"  // auto-generated: ../wxui/viewdetails_base.h and ..//wxui/viewdetails_base.cpp
 
 // If this constructor is used, the caller must call Create(parent)
 ViewDetails::ViewDetails() {}
 
 ViewDetails::ViewDetails(wxWindow* parent)
 {
-    Create(parent);
+    (void) Create(parent);
 }
 
 void ViewDetails::OnInit(wxInitDialogEvent& event)
 {
     event.Skip();  // transfer all validator data to their windows and update UI
 }
-
-// ************* End of generated code ***********
-// DO NOT EDIT THIS COMMENT BLOCK!
-//
-// Code below this comment block will be preserved
-// if the code for this class is re-generated.
-//
-// NOLINTEND
-// cppcheck-suppress-end *
-// ***********************************************
-// </auto-generated>

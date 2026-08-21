KeyWorksRW/wxUiEditor

commit 3bed9527c98d46a1ffd9dd636e4b01ba666ed5c3

Add 'unused' comment around unused parameters

src/generate/gen_text_ctrl.cpp
@@ -61,7 +61,7 @@ wxObject* TextCtrlGenerator::CreateMockup(Node* node, wxObject* parent)
     return widget;
 }
 
-bool TextCtrlGenerator::OnPropertyChange(wxObject* widget, Node* node, NodeProperty* prop)
+bool TextCtrlGenerator::OnPropertyChange(wxObject* widget, Node* /* node unused */, NodeProperty* prop)
 {
     if (prop->isProp(prop_value))
     {

src/generate/gen_web_view.cpp
@@ -17,7 +17,7 @@
 
 #include "gen_web_view.h"
 
-wxObject* WebViewGenerator::CreateMockup(Node* node, wxObject* parent)
+wxObject* WebViewGenerator::CreateMockup(Node* /* node unused */, wxObject* parent)
 {
     if (Project.get_CodePreference() == GenLang::ruby ||
         Project.get_CodePreference() == GenLang::xrc)

src/panels/language_docs.cpp
@@ -225,7 +225,7 @@ void LanguageDocs::OnForward(wxCommandEvent& /* event unused */)
 #endif
 }
 
-void LanguageDocs::OnUpdateBack(wxUpdateUIEvent& event)
+void LanguageDocs::OnUpdateBack(wxUpdateUIEvent& /* event unused */)
 {
 #if wxUSE_WEBVIEW
     if (!m_webview)
@@ -251,7 +251,7 @@ void LanguageDocs::OnUpdateBack(wxUpdateUIEvent& event)
 #endif
 }
 
-void LanguageDocs::OnUpdateForward(wxUpdateUIEvent& event)
+void LanguageDocs::OnUpdateForward(wxUpdateUIEvent& /* event unused */)
 {
 #if wxUSE_WEBVIEW
     if (!m_webview)

commit fa9178ce78371009f33a36240b1d2c113c3518cc

This fixes a build regression from a previous commit (3bed952)
Switched to [[maybe_unused]] for parameters that are only used in some, but not all, platforms

src/generate/gen_text_ctrl.cpp
@@ -61,7 +61,8 @@ wxObject* TextCtrlGenerator::CreateMockup(Node* node, wxObject* parent)
     return widget;
 }
 
-bool TextCtrlGenerator::OnPropertyChange(wxObject* widget, Node* /* node unused */, NodeProperty* prop)
+bool TextCtrlGenerator::OnPropertyChange(wxObject* widget, [[maybe_unused]] Node* node,
+                                         NodeProperty* prop)
 {
     if (prop->isProp(prop_value))
     {

src/generate/gen_web_view.cpp
@@ -17,7 +17,7 @@
 
 #include "gen_web_view.h"
 
-wxObject* WebViewGenerator::CreateMockup(Node* /* node unused */, wxObject* parent)
+wxObject* WebViewGenerator::CreateMockup([[maybe_unused]] Node* node, wxObject* parent)
 {
     if (Project.get_CodePreference() == GenLang::ruby ||
         Project.get_CodePreference() == GenLang::xrc)

src/panels/language_docs.cpp
@@ -73,7 +73,7 @@ void LanguageDocs::ActivatePage()
     }
 }
 
-void LanguageDocs::OnNodeSelected(CustomEvent& /* event unused */)
+void LanguageDocs::OnNodeSelected([[maybe_unused]] CustomEvent& event)
 {
     if (!IsShown())
     {
@@ -199,12 +199,12 @@ void LanguageDocs::OnRuby(wxCommandEvent& /* event */)
 #endif
 }
 
-void LanguageDocs::OnHome(wxCommandEvent& /* event unused */)
+void LanguageDocs::OnHome([[maybe_unused]] wxCommandEvent& event)
 {
     ActivatePage();
 }
 
-void LanguageDocs::OnBack(wxCommandEvent& /* event unused */)
+void LanguageDocs::OnBack([[maybe_unused]] wxCommandEvent& event)
 {
 #if wxUSE_WEBVIEW
     if (!m_webview)
@@ -215,7 +215,7 @@ void LanguageDocs::OnBack(wxCommandEvent& /* event unused */)
 #endif
 }
 
-void LanguageDocs::OnForward(wxCommandEvent& /* event unused */)
+void LanguageDocs::OnForward([[maybe_unused]] wxCommandEvent& event)
 {
 #if wxUSE_WEBVIEW
     if (!m_webview)
@@ -225,7 +225,7 @@ void LanguageDocs::OnForward(wxCommandEvent& /* event unused */)
 #endif
 }
 
-void LanguageDocs::OnUpdateBack(wxUpdateUIEvent& /* event unused */)
+void LanguageDocs::OnUpdateBack([[maybe_unused]] wxUpdateUIEvent& event)
 {
 #if wxUSE_WEBVIEW
     if (!m_webview)
@@ -251,7 +251,7 @@ void LanguageDocs::OnUpdateBack(wxUpdateUIEvent& /* event unused */)
 #endif
 }
 
-void LanguageDocs::OnUpdateForward(wxUpdateUIEvent& /* event unused */)
+void LanguageDocs::OnUpdateForward([[maybe_unused]] wxUpdateUIEvent& event)
 {
 #if wxUSE_WEBVIEW
     if (!m_webview)